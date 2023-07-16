a7937b9b5a51a7e472e6b7b7ae90178ce801a4d3
iyyafi <iyyafi@gmail.com>
2023-07-16 08:19:47 +0700

    update component name

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,85 +1,85 @@
 /* eslint-disable */
-import { useParams,  Link as RouterLink, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useParams, Link as RouterLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
 // form
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 // @mui
-import { Stack, IconButton, InputAdornment, Card, Link, Container, Typography } from '@mui/material';
-import { LoadingButton } from '@mui/lab';
+import { Stack, IconButton, InputAdornment, Card, Link, Container, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 // @mui
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 // components
-import axios from 'axios';
-import Iconify from '../components/Iconify';
-import Page from '../components/Page';
-import Logo from '../components/Logo'
-import Collapse from '@mui/material/Collapse';
-import Alert from '@mui/material/Alert';
+import axios from "axios";
+import Iconify from "../components/Iconify";
+import Page from "../components/Page";
+import Logo from "../components/Logo";
+import Collapse from "@mui/material/Collapse";
+import Alert from "@mui/material/Alert";
 // hooks
-import useResponsive from '../hooks/useResponsive';
+import useResponsive from "../hooks/useResponsive";
 // sections
-import { FormProvider, RHFTextField } from '../components/hook-form';
+import { FormProvider, RHFTextField } from "../components/hook-form";
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({ theme }) => ({
-  [theme.breakpoints.up('md')]: {
-    display: 'flex',
+const RootStyle = styled("div")(({ theme }) => ({
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
   },
 }));
 
-const HeaderStyle = styled('header')(({ theme }) => ({
+const HeaderStyle = styled("header")(({ theme }) => ({
   top: 0,
   zIndex: 9,
   lineHeight: 0,
-  width: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  position: 'absolute',
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  position: "absolute",
   padding: theme.spacing(3),
-  justifyContent: 'space-between',
-  [theme.breakpoints.up('md')]: {
-    alignItems: 'flex-start',
+  justifyContent: "space-between",
+  [theme.breakpoints.up("md")]: {
+    alignItems: "flex-start",
     padding: theme.spacing(7, 5, 0, 7),
   },
 }));
 
 const SectionStyle = styled(Card)(({ theme }) => ({
-  width: '100%',
+  width: "100%",
   maxWidth: 464,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
   margin: theme.spacing(2, 0, 2, 2),
 }));
 
-const ContentStyle = styled('div')(({ theme }) => ({
+const ContentStyle = styled("div")(({ theme }) => ({
   maxWidth: 480,
-  margin: 'auto',
-  minHeight: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
+  margin: "auto",
+  minHeight: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
   padding: theme.spacing(12, 0),
 }));
 
 // ----------------------------------------------------------------------
 
-export default function ForgotPassword() {
-  const smUp = useResponsive('up', 'sm');
+export default function ResetPassword() {
+  const smUp = useResponsive("up", "sm");
 
-  const mdUp = useResponsive('up', 'md');
+  const mdUp = useResponsive("up", "md");
 
   const navigate = useNavigate();
 
-  let {token} = useParams()
+  let { token } = useParams();
 
-  const [openAlert, setOpenAlert]       = useState(false);
+  const [openAlert, setOpenAlert] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
-  const [password, setPassword]         = useState('');
-  
+  const [password, setPassword] = useState("");
+
   const defaultValues = {
-    password: '',
+    password: "",
     remember: true,
   };
 
@@ -93,19 +93,21 @@ export default function ForgotPassword() {
   } = methods;
 
   const onSubmit = () => {
-    console.log(token)
+    console.log(token);
     const data = {
-      password
-    }
-    axios.post(`${process.env.REACT_APP_BASE_URL}/api/resetpassword/${token}`, data)
-      .then(res => {
+      password,
+    };
+    axios
+      .post(`${process.env.REACT_APP_BASE_URL}/api/resetpassword/${token}`, data)
+      .then((res) => {
         if (res.status === 200) {
           //navigate('/login', { replace: true });
-          setOpenAlert(true)
+          setOpenAlert(true);
         }
         console.log(res);
-        console.log(res.data); 
-      }).catch((error) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
         if (error.response) {
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
@@ -113,11 +115,10 @@ export default function ForgotPassword() {
           console.log(error.request);
         } else {
           // Something happened in setting up the request that triggered an Error
-          console.log('Error', error.message);
+          console.log("Error", error.message);
         }
         console.log(error.config);
       });
-    
   };
 
   return (
@@ -128,7 +129,7 @@ export default function ForgotPassword() {
 
           {smUp && (
             <Typography variant="body2" sx={{ mt: { md: -2 } }}>
-              Don’t have an account? {''}
+              Don’t have an account? {""}
               <Link variant="subtitle2" component={RouterLink} to="/register">
                 Get started
               </Link>
@@ -148,15 +149,12 @@ export default function ForgotPassword() {
         <Container maxWidth="sm">
           <ContentStyle>
             <Typography variant="h4" gutterBottom>
-              Update Password 
+              Update Password
             </Typography>
 
-            <Typography sx={{ color: 'text.secondary', mb: 5 }}>Enter new password below.</Typography>
+            <Typography sx={{ color: "text.secondary", mb: 5 }}>Enter new password below.</Typography>
             <Collapse in={openAlert}>
-              <Alert
-                severity="success"
-                sx={{ mb: 2 }}
-              >
+              <Alert severity="success" sx={{ mb: 2 }}>
                 <Typography variant="body2">
                   Reset Password berhasil,
                   <Link variant="subtitle2" component={RouterLink} to="/login">
@@ -166,17 +164,20 @@ export default function ForgotPassword() {
               </Alert>
             </Collapse>
             <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-              <Stack spacing={3} sx={{mb:2}} >
+              <Stack spacing={3} sx={{ mb: 2 }}>
                 <RHFTextField
                   name="password"
                   label="Password"
-                  value={password} onChange={(e) => {setPassword(e.target.value)}}
-                  type={showPassword ? 'text' : 'password'}
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  type={showPassword ? "text" : "password"}
                   InputProps={{
                     endAdornment: (
                       <InputAdornment position="end">
                         <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                          <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                          <Iconify icon={showPassword ? "eva:eye-fill" : "eva:eye-off-fill"} />
                         </IconButton>
                       </InputAdornment>
                     ),
@@ -191,7 +192,7 @@ export default function ForgotPassword() {
 
             {!smUp && (
               <Typography variant="body2" align="center" sx={{ mt: 3 }}>
-                Don’t have an account?{' '}
+                Don’t have an account?{" "}
                 <Link variant="subtitle2" component={RouterLink} to="/register">
                   Get started
                 </Link>
