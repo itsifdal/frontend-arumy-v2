e4fd6095726c1f805b95e54976af76f4e20cf570
iyyafi <iyyafi@gmail.com>
2023-07-18 13:12:07 +0700

    refactoring routing

--- src/routes.js
+++ src/routes.js
@@ -1,82 +1,81 @@
-import { Navigate, useRoutes } from 'react-router-dom';
+import { Navigate, useRoutes } from "react-router-dom";
 
 // layouts
-import Login from './pages/Login';
-import Register from './pages/Register';
-import DashboardLayout from './layouts/dashboard';
-import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import DashboardLayout from "./layouts/dashboard";
+import LogoOnlyLayout from "./layouts/LogoOnlyLayout";
 //
-import Room from './pages/Room';
-import Post from './pages/Post';
-import PostDetail from './pages/PostDetail';
-import AddPost from './pages/AddPost';
-import AddBooking from './pages/AddBooking';
-import UpdateBooking from './pages/UpdateBooking';
-import UpdatePost from './pages/UpdatePost';
-import Booking from './pages/Booking';
-import UpdateBookingPage from './pages/UpdateBookingPage';
+import Room from "./pages/Room";
+import Post from "./pages/Post";
+import PostDetail from "./pages/PostDetail";
+import AddPost from "./pages/AddPost";
+import AddBooking from "./pages/AddBooking";
+import UpdateBooking from "./pages/UpdateBooking";
+import UpdatePost from "./pages/UpdatePost";
+import Booking from "./pages/Booking";
+import UpdateBookingPage from "./pages/UpdateBookingPage";
 
-import Rooms from './pages/Rooms';
-import User from './pages/User';
-import ForgotPassword from './pages/ForgotPassword';
-import ResetPassword from './pages/ResetPassword';
-import NotFound from './pages/Page404';
-import DashboardApp from './pages/DashboardApp';
+import Rooms from "./pages/Rooms";
+import User from "./pages/User";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import NotFound from "./pages/Page404";
+import DashboardApp from "./pages/DashboardApp";
 
 // ----------------------------------------------------------------------
 
 export default function Router() {
-
   return useRoutes([
     {
-      path: '/login',
+      path: "/login",
       element: <Login />,
     },
     {
-      path: '/register',
+      path: "/register",
       element: <Register />,
     },
     {
-      path: '/dashboard',
+      path: "/dashboard",
       element: <DashboardLayout />,
       children: [
-        { path: 'app', element: <DashboardApp /> },
-        { path: 'user', element: <User /> },
-        { path: 'room', element: <Room /> },
-        { path: 'booking', element: <Booking /> },
-        { path: 'updateBookingPage/:id', element: <UpdateBookingPage /> },
-        { path: 'post', element: <Post /> },
-        { path: 'rooms', element: <Rooms /> },
-        { path: 'addpost', element: <AddPost /> },
-        { path: 'updatepost/:slug', element: <UpdatePost /> },
-        { path: 'postdetail/:slug', element: <PostDetail /> },
-        { path: 'addbooking', element: <AddBooking /> },
-        { path: 'updatebooking/:id', element: <UpdateBooking /> }
+        { path: "app", element: <DashboardApp /> },
+        { path: "user", element: <User /> },
+        { path: "room", element: <Room /> },
+        { path: "booking", element: <Booking /> },
+        { path: "updateBookingPage/:id", element: <UpdateBookingPage /> },
+        { path: "post", element: <Post /> },
+        { path: "rooms", element: <Rooms /> },
+        { path: "addpost", element: <AddPost /> },
+        { path: "updatepost/:slug", element: <UpdatePost /> },
+        { path: "postdetail/:slug", element: <PostDetail /> },
+        { path: "addbooking", element: <AddBooking /> },
+        { path: "updatebooking/:id", element: <UpdateBooking /> },
       ],
     },
     {
-      path: 'forgotpassword',
+      path: "/forgotpassword",
       element: <ForgotPassword />,
     },
     {
-      path: 'resetpassword/:token',
+      path: "/resetpassword/:token",
       element: <ResetPassword />,
     },
     {
-      path: 'postdetail/:slug',
+      path: "/postdetail/:slug",
       element: <PostDetail />,
     },
     {
-      path: '/',
+      path: "/",
       element: <LogoOnlyLayout />,
       children: [
-        { path: '/', element: <Navigate to="/dashboard/app" /> },
-        { path: '404', element: <NotFound /> },
-        { path: '*', element: <Navigate to="/404" /> },
+        { path: "/", element: <Navigate to="/dashboard/app" /> },
+        { path: "404", element: <NotFound /> },
+        { path: "*", element: <Navigate to="/404" /> },
       ],
     },
     {
-      path: '*',
+      path: "*",
       element: <Navigate to="/404" replace />,
     },
   ]);
