8ab2237297766ad792151041302402b67c556f6e
iyyafi <iyyafi@gmail.com>
2023-08-30 20:31:48 +0700

    update dashboard timeline

--- src/pages/DashboardTimeline.js
+++ src/pages/DashboardTimeline.js
@@ -3,23 +3,26 @@ import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { format, parse } from "date-fns";
 import axios from "axios";
-import { Scheduler } from "@aldabil/react-scheduler";
 import randomColor from "randomcolor";
 import id from "date-fns/locale/id";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
 // material
-import { Stack, Button, Container, Typography, Box, Grid } from "@mui/material";
+import { Stack, Button, Container, Box, Grid } from "@mui/material";
 // components
 import { queryKey } from "../constants/queryKey";
 import Page from "../components/Page";
 // sections
 import PageHeader from "../components/PageHeader";
 import DateInputBasic from "../components/input/dateInputBasic";
+import AutoCompleteBasic from "../components/input/autoCompleteBasic";
 import { urlSearchParamsToQuery } from "../utils/urlSearchParamsToQuery";
 import { queryToString } from "../utils/queryToString";
 import { cleanQuery } from "../utils/cleanQuery";
 
 const initFilter = {
   tgl_kelas: format(new Date(), "yyyy-MM-dd"),
+  roomId: 21,
 };
 
 // ----------------------------------------------------------------------
@@ -28,31 +31,29 @@ export default function DashboardTimeline() {
   const navigate = useNavigate();
   const [foundUser, setFoundUser] = useState(true);
   const [filters, setFilters] = useState(initFilter);
+  const [openRoom, setOpenRoom] = useState(false);
 
   const [searchParams, setSearchParams] = useSearchParams();
   const queryParam = urlSearchParamsToQuery(searchParams);
 
-  const { data: rooms = [] } = useQuery(
+  const todayStr = new Date().toISOString().replace(/T.*$/, "");
+
+  const { data: rooms = [], isLoading: isLoadingRooms } = useQuery(
     [queryKey.rooms],
     () => axios.get(`${process.env.REACT_APP_BASE_URL}/api/room`).then((res) => res.data),
     {
-      select: (roomList) =>
-        roomList.map((room) => ({
-          room_id: room.id,
-          title: room.nama_ruang,
-          branch: room.cabang.nama_cabang,
-          color: randomColor({
-            luminosity: "dark",
-          }),
-        })),
+      select: (roomList) => roomList.map((room) => ({ value: room.id, label: room.nama_ruang })),
+      enabled: openRoom,
     }
   );
 
   const defaultQueryBooking = {
     tgl_kelas: format(new Date(), "yyyy-MM-dd"),
+    roomId: 21,
     ...queryParam,
     perPage: 9999,
   };
+
   const {
     data: bookings = [],
     isLoading: isLoadingBookings,
@@ -66,16 +67,16 @@ export default function DashboardTimeline() {
     {
       select: (bookingList) =>
         bookingList.data.map((booking) => ({
-          event_id: booking.id,
+          id: booking.id,
           title: JSON.parse(booking.user_group)
             .map((student) => student.nama_murid)
             .join(", "),
-          start: parse(booking.jam_booking, "HH:mm:ss", new Date()),
-          end: parse(booking.selesai, "HH:mm:ss", new Date()),
+          start: `${booking.tgl_kelas}T${booking.jam_booking}`,
+          end: `${booking.tgl_kelas}T${booking.selesai}`,
           color: randomColor({
             luminosity: "dark",
           }),
-          room_id: booking.roomId,
+          // date: "2020-07-30"
         })),
     }
   );
@@ -102,40 +103,7 @@ export default function DashboardTimeline() {
     setFilters((prevState) => ({ ...prevState, ...filter }));
     setSearchParams({ ...filters, ...filter });
   };
-
-  function renderContent() {
-    if (isLoadingBookings) return <Typography>Loading Data</Typography>;
-
-    if (!bookings.length) return <Typography>No Data Found</Typography>;
-
-    return (
-      <Scheduler
-        events={bookings}
-        resources={rooms}
-        resourceViewMode={"tabs"}
-        resourceFields={{
-          idField: "room_id",
-          textField: "title",
-          subTextField: "branch",
-          colorField: "color",
-        }}
-        locale={id}
-        hourFormat={24}
-        view="day"
-        day={{
-          startHour: 8,
-          endHour: 17,
-          step: 60,
-          navigation: false,
-        }}
-        disableViewNavigator
-        editable={false}
-        deletable={false}
-        draggable={false}
-        onEventClick={() => {}}
-      />
-    );
-  }
+  console.log(bookings);
 
   return (
     <Page title="Dashboard">
@@ -164,6 +132,25 @@ export default function DashboardTimeline() {
           <Stack width={"100%"} direction={"row"} spacing={2}>
             <Grid container spacing={1} flexGrow={1}>
               <Grid item xs={4}>
+                <AutoCompleteBasic
+                  label="Ruang Kelas"
+                  name="roomId"
+                  open={openRoom}
+                  onOpen={() => {
+                    setOpenRoom(true);
+                  }}
+                  onClose={() => {
+                    setOpenRoom(false);
+                  }}
+                  onChange={(_, newValue) => {
+                    SubmitFilter({ roomId: newValue?.value || 21, roomLabel: newValue?.label || "Online" });
+                  }}
+                  options={rooms}
+                  loading={isLoadingRooms}
+                  value={{ value: filters.roomId || 21, label: filters.roomLabel || "Online" }}
+                />
+              </Grid>
+              <Grid item xs={4}>
                 <DateInputBasic
                   disableValidation
                   id="tgl_kelas"
@@ -180,7 +167,27 @@ export default function DashboardTimeline() {
         </Container>
       </Box>
       <Container maxWidth="xl" sx={{ paddingTop: 4 }}>
-        {renderContent()}
+        {!isLoadingBookings ? (
+          <FullCalendar
+            plugins={[timeGridPlugin]}
+            initialView="timeGridDay"
+            headerToolbar={{ left: "", center: "", right: "" }}
+            slotMinTime="08:00:00"
+            slotMaxTime="18:00:00"
+            nowIndicator
+            now={parse(filters.tgl_kelas, "yyyy-MM-dd", new Date()) || new Date()}
+            height="620px"
+            locale={id}
+            slotLabelFormat={{
+              hour: "numeric",
+              minute: "2-digit",
+              omitZeroMinute: false,
+              meridiem: "short",
+            }}
+            initialEvents={bookings}
+            allDaySlot={false}
+          />
+        ) : null}
       </Container>
     </Page>
   );
