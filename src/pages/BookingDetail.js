d41902e74f563df3e48eca9802fb7c88c53517ba
iyyafi <iyyafi@gmail.com>
2023-09-29 08:14:52 +0700

    add confirm button in detail

--- src/pages/BookingDetail.js
+++ src/pages/BookingDetail.js
@@ -1,10 +1,11 @@
 /* eslint-disable camelcase */
-import React from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Stack, Button, Typography, Box, Grid, Avatar, Chip, IconButton } from "@mui/material";
 import { format, parse } from "date-fns";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 import Page from "../components/Page";
 import PageHeader from "../components/PageHeader";
@@ -12,6 +13,7 @@ import { stringAvatar } from "../utils/avatarProps";
 import Iconify from "../components/Iconify";
 import { bookingStatusObj } from "../constants/bookingStatus";
 import { queryKey } from "../constants/queryKey";
+import ConfirmBooking from "../components/modal/confirmBooking";
 
 // ----------------------------------------------------------------------
 const generateStatus = (status) => {
@@ -50,12 +52,15 @@ export default function BookingDetail() {
 
 function BookingData() {
   const { id } = useParams();
+  const queryClient = useQueryClient();
+  const [openUpdStatus, setOpenUpdStatus] = useState(false);
   const {
     data: booking,
     isLoading,
     isError,
+    refetch: refetchBooking,
   } = useQuery(
-    [queryKey.bookings, "DETAIL"],
+    [queryKey.bookings, "DETAIL", id],
     () => axios.get(`${process.env.REACT_APP_BASE_URL}/api/booking/${id}`).then((res) => res.data),
     {
       enabled: Boolean(id),
@@ -64,80 +69,109 @@ function BookingData() {
 
   if (isLoading) return <Typography>Loading data...</Typography>;
   if (isError) return <Typography>Error data</Typography>;
+
+  const bookingDetail = [
+    {
+      label: "DATE",
+      value: <strong>{format(parse(booking.tgl_kelas, "yyyy-MM-dd", new Date()), "MMMM dd, yyyy")}</strong>,
+    },
+    { label: "BRANCH", value: booking.cabang },
+    { label: "START", value: format(parse(booking.jam_booking, "HH:mm:ss", new Date()), "HH:mm") },
+    { label: "END", value: format(parse(booking.selesai, "HH:mm:ss", new Date()), "HH:mm") },
+    { label: "DURATION", value: <strong>{booking.durasi} Minutes</strong> },
+    { label: "CLASS TYPE", value: booking.jenis_kelas },
+    { label: "STATUS", value: generateStatus(booking.status) },
+  ];
+
+  const onClickConfirm = () => {
+    setOpenUpdStatus(true);
+  };
+  const handleCloseModalUpdateStatus = () => setOpenUpdStatus(false);
+  const onSuccessMutateBooking = (response) => {
+    setOpenUpdStatus(false);
+    refetchBooking();
+    queryClient.invalidateQueries({ queryKey: [queryKey.bookings] });
+    toast.success(response.data.message, {
+      position: "top-center",
+      autoClose: 1000,
+      theme: "colored",
+    });
+  };
+  const onErrorMutateBooking = (error) => {
+    if (error) {
+      toast.error("Booking Error", {
+        position: "top-center",
+        autoClose: 1000,
+        theme: "colored",
+      });
+    }
+  };
+
   return (
-    <Stack
-      borderRadius={"7px"}
-      boxShadow={"2px 12px 20px 0px rgba(90, 117, 167, 0.10)"}
-      padding={"15px"}
-      sx={{
-        background: "white",
-      }}
-      gap={"11px"}
-    >
-      <Stack direction={"row"} gap={"10px"}>
-        <Avatar {...stringAvatar("Privat")} sx={{ width: 48, height: 48 }} />
-        <Stack gap={"2px"} width={"100%"}>
-          <Typography fontWeight={"bold"} fontSize={"14px"} color={"#0D1B34"}>
-            {booking.user_group.map((student) => student.nama_murid).join(", ")}
-          </Typography>
-          <Stack direction={"row"} justifyContent={"space-between"}>
+    <Stack gap={3}>
+      <Stack
+        borderRadius={"7px"}
+        boxShadow={"2px 12px 20px 0px rgba(90, 117, 167, 0.10)"}
+        padding={"15px"}
+        sx={{
+          background: "white",
+        }}
+        gap={"11px"}
+      >
+        <Stack direction={"row"} gap={"10px"}>
+          <Avatar {...stringAvatar("Privat")} sx={{ width: 48, height: 48 }} />
+          <Stack gap={"2px"} width={"100%"}>
+            <Typography fontWeight={"bold"} fontSize={"14px"} color={"#0D1B34"}>
+              {booking.user_group.map((student) => student.nama_murid).join(", ")}
+            </Typography>
             <Typography fontSize={"14px"} color={"#8696BB"}>
               {booking.room.nama_ruang}
             </Typography>
-            {generateStatus(booking.status)}
           </Stack>
         </Stack>
+        <Box width={"100%"} height={"1px"} sx={{ background: "#F5F5F5" }} />
+        <ToastContainer pauseOnFocusLoss={false} />
+        {bookingDetail.map((detail) => (
+          <Grid container key={detail.label}>
+            <Grid item xs={4}>
+              <Typography fontSize={"14px"} color={"#8696BB"}>
+                {detail.label}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography fontSize={"14px"} color={"#525771"}>
+                {detail.value}
+              </Typography>
+            </Grid>
+          </Grid>
+        ))}
       </Stack>
-      <Box width={"100%"} height={"1px"} sx={{ background: "#F5F5F5" }} />
-      <Grid container>
-        <Grid item xs={6}>
-          <Stack direction={"row"} gap={"6px"}>
-            <Iconify icon="ic:round-date-range" sx={{ width: "16px", height: "16px", color: "#8696BB" }} />
-            <Typography fontSize={"12px"} color={"#8696BB"}>
-              {format(parse(booking.tgl_kelas, "yyyy-MM-dd", new Date()), "iiii, dd MMM")}
-            </Typography>
-          </Stack>
-        </Grid>
-        <Grid item xs={6}>
-          <Stack direction={"row"} gap={"6px"}>
-            <Iconify icon="tabler:clock" sx={{ width: "16px", height: "16px", color: "#8696BB" }} />
-            <Typography fontSize={"12px"} color={"#8696BB"}>
-              {`${format(parse(booking.jam_booking, "HH:mm:ss", new Date()), "HH:mm")} - 
-          ${format(parse(booking.selesai, "HH:mm:ss", new Date()), "HH:mm")}`}
-            </Typography>
-          </Stack>
-        </Grid>
-      </Grid>
-      <Stack direction={"row"} justifyContent={"space-around"}>
+      {booking.status !== "konfirmasi" && booking.status !== "batal" ? (
         <Button
+          onClick={onClickConfirm}
+          data-id={booking.id}
+          fullWidth
           sx={{
-            width: "117px",
-            height: "27px",
             padding: "12px 32px",
             borderRadius: "7px",
-            background: "rgba(255, 165, 0, 0.20)",
-            color: "#FFA500",
+            background: "#19A551",
+            color: "#FFF",
           }}
-          {...(booking.status !== "konfirmasi" && booking.status !== "batal" && { fullWidth: true })}
         >
-          Detail
+          Masuk
         </Button>
-        {booking.status !== "konfirmasi" && booking.status !== "batal" ? (
-          <Button
-            data-id={booking.id}
-            sx={{
-              width: "117px",
-              height: "27px",
-              padding: "12px 32px",
-              borderRadius: "7px",
-              background: "#19A551",
-              color: "#FFF",
-            }}
-          >
-            Masuk
-          </Button>
-        ) : null}
-      </Stack>
+      ) : null}
+      <ConfirmBooking
+        open={openUpdStatus}
+        onClose={handleCloseModalUpdateStatus}
+        id={Number(id)}
+        callbackSuccess={(response) => {
+          onSuccessMutateBooking(response);
+        }}
+        callbackError={(error) => {
+          onErrorMutateBooking(error);
+        }}
+      />
     </Stack>
   );
 }
