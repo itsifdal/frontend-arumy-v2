ffcd17d7ebd7c6c4b320e43c23664bdd541ba7c0
Iyyafi Qolbi <iyyafi@gmail.com>
2023-10-04 18:52:02 +0700

    update filter view

--- src/components/filter/bookingFilters.js
+++ src/components/filter/bookingFilters.js
@@ -6,6 +6,7 @@ import { format, parse, isValid } from "date-fns";
 import PropTypes from "prop-types";
 import { Button, Stack, Grid, Drawer, IconButton, Box, ToggleButtonGroup, ToggleButton } from "@mui/material";
 
+import useResponsive from "../../hooks/useResponsive";
 import AutoCompleteBasic from "../input/autoCompleteBasic";
 import DateInputBasic from "../input/dateInputBasic";
 import NativeSelectBasic from "../input/nativeSelectBasic";
@@ -22,15 +23,17 @@ const initFilter = {
   studentId: "",
   studentLabel: "",
   teacherId: "",
+  teacherLabel: "",
 };
 
 export default function BookingFilters({ toggleValue }) {
   const [openDrawer, setOpenDrawer] = useState(false);
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const [filters, setFilters] = useState(initFilter);
   const filterString = useMemo(
     () => [
       filters.studentLabel,
+      filters.teacherLabel,
       filters.roomLabel,
       filters.tgl_kelas && format(parse(filters.tgl_kelas, "yyyy-MM-dd", new Date()), "d MMM yyyy"),
       filters.status && `Status ${bookingStatusObj[filters.status].label}`,
@@ -38,6 +41,7 @@ export default function BookingFilters({ toggleValue }) {
     [filters]
   );
   const navigate = useNavigate();
+  const isDesktop = useResponsive("up", "lg");
 
   const handleChange = (event, newValue) => {
     if (!newValue) {
@@ -60,21 +64,44 @@ export default function BookingFilters({ toggleValue }) {
     setOpenDrawer(open);
   };
 
+  const ResetFilter = () => {
+    if (toggleDrawer) toggleDrawer(false);
+    setSearchParams();
+  };
+
   return (
     <>
-      <Stack justifyContent={"flex-end"} direction={"row"} gap={2}>
-        <ToggleButtonGroup value={toggleValue} exclusive onChange={handleChange} aria-label="Platform">
+      <Stack justifyContent={"flex-end"} direction={"row"} gap={isDesktop ? 2 : 1} flexWrap={"wrap"}>
+        <ToggleButtonGroup
+          value={toggleValue}
+          exclusive
+          onChange={handleChange}
+          aria-label="Platform"
+          {...(!isDesktop && { fullWidth: true })}
+        >
           <ToggleButton value="upcoming">Upcoming</ToggleButton>
           <ToggleButton value="past">Past</ToggleButton>
         </ToggleButtonGroup>
-        <Button variant="outlined" startIcon={<Iconify icon="mdi:filter-outline" />} onClick={toggleDrawer(true)}>
-          {filterString.filter((element) => element !== undefined).length
-            ? `${filterString.filter((element) => element !== undefined).join(", ")}`
-            : "Filter"}
-        </Button>
+        <Stack direction={"row"} width={isDesktop ? "auto" : "100%"} justifyContent={"flex-end"}>
+          <Button
+            variant="outlined"
+            startIcon={<Iconify icon="mdi:filter-outline" />}
+            onClick={toggleDrawer(true)}
+            sx={{ lineHeight: 1.2, ...(!isDesktop && { width: "100%" }) }}
+          >
+            {filterString.filter((element) => element !== undefined).length
+              ? `${filterString.filter((element) => element !== undefined).join(", ")}`
+              : "Filter"}
+          </Button>
+          {filterString.filter((element) => element !== undefined).length ? (
+            <IconButton aria-label="clear" onClick={ResetFilter}>
+              <Iconify icon="ic:round-close" sx={{ width: "20px", height: "20px" }} />
+            </IconButton>
+          ) : null}
+        </Stack>
       </Stack>
       <Drawer anchor={"right"} open={openDrawer} onClose={toggleDrawer(false)}>
-        <Stack padding={1} paddingRight={3} direction={"row"} alignItems={"flex-start"}>
+        <Stack padding={2} gap={1} paddingRight={4} direction={"row"} alignItems={"flex-start"}>
           <IconButton aria-label="back" onClick={toggleDrawer(false)}>
             <Iconify icon="ph:arrow-left-bold" sx={{ width: "20px", height: "20px" }} />
           </IconButton>
@@ -93,6 +120,7 @@ BookingFilters.propTypes = {
 function BookingFilterForm({ toggleDrawer }) {
   const [openRoom, setOpenRoom] = useState(false);
   const [openStudent, setOpenStudent] = useState(false);
+  const [openTeacher, setOpenTeacher] = useState(false);
   const [filters, setFilters] = useState(initFilter);
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -102,6 +130,15 @@ function BookingFilterForm({ toggleDrawer }) {
     {
       select: (students) => students.data?.map((student) => ({ value: student.id, label: student.nama_murid })),
       enabled: openStudent,
+    }
+  );
+
+  const { data: teachers = [], isLoading: isLoadingTeachers } = useQuery(
+    [queryKey.teachers],
+    () => axios.get(`${process.env.REACT_APP_BASE_URL}/api/teacher?perPage=9999`).then((res) => res.data),
+    {
+      select: (teachers) => teachers.data?.map((teacher) => ({ value: teacher.id, label: teacher.nama_pengajar })),
+      enabled: openTeacher,
     }
   );
 
@@ -131,7 +168,7 @@ function BookingFilterForm({ toggleDrawer }) {
 
   return (
     <Stack width={"100%"} direction={"column"} spacing={2}>
-      <Grid container flexGrow={1} gap={1}>
+      <Grid container flexGrow={1} gap={2}>
         <Grid item xs={12}>
           <AutoCompleteBasic
             label="Nama Murid"
@@ -153,6 +190,29 @@ function BookingFilterForm({ toggleDrawer }) {
             options={students}
             loading={isLoadingStudents}
             value={{ value: filters.studentId || "", label: filters.studentLabel || "" }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <AutoCompleteBasic
+            label="Nama Pengajar"
+            name="teacherId"
+            open={openTeacher}
+            onOpen={() => {
+              setOpenTeacher(true);
+            }}
+            onClose={() => {
+              setOpenTeacher(false);
+            }}
+            onChange={(_, newValue) => {
+              setFilters((prevState) => ({
+                ...prevState,
+                teacherId: newValue?.value || "",
+                teacherLabel: newValue?.label || "",
+              }));
+            }}
+            options={teachers}
+            loading={isLoadingTeachers}
+            value={{ value: filters.teacherId || "", label: filters.teacherLabel || "" }}
           />
         </Grid>
         <Grid item xs={12}>
