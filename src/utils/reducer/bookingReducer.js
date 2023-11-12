401ee5a60e9a297da6c87f45758918f2eb98d55f
Iyyafi Qolbi <iyyafi@gmail.com>
2023-11-12 20:15:41 +0700

    add group validation

--- src/utils/reducer/bookingReducer.js
+++ src/utils/reducer/bookingReducer.js
@@ -34,34 +34,31 @@ export const initialBookingFormState = {
 export function validateBookingForm(values) {
   const errors = {};
   if (!values.roomId) {
-    errors.roomId = "roomId is required";
+    errors.roomId = "Ruangan wajib diisi";
   }
   if (!values.teacherId) {
-    errors.teacherId = "teacherId is required";
+    errors.teacherId = "Pengajar wajib diisi";
   }
   if (!values.instrumentId) {
-    errors.instrumentId = "instrumentId is required";
+    errors.instrumentId = "Alat musik wajib diisi";
   }
   if (!values.user_group) {
-    errors.user_group = "user_group is required";
+    errors.user_group = "Murid wajib diisi";
   }
   if (!values.tgl_kelas) {
-    errors.tgl_kelas = "tgl_kelas is required";
-  }
-  if (!values.cabang) {
-    errors.cabang = "cabang is required";
+    errors.tgl_kelas = "Tanggal kelas wajib diisi";
   }
   if (!values.jam_booking) {
-    errors.jam_booking = "jam_booking is required";
+    errors.jam_booking = "Jam mulai kelas wajib diisi";
   }
   if (!values.jam_selesai_booking) {
-    errors.jam_selesai_booking = "jam_selesai_booking is required";
+    errors.jam_selesai_booking = "Jam selesai kelas wajib diisi";
   }
   if (!values.jenis_kelas) {
-    errors.jenis_kelas = "jenis_kelas is required";
+    errors.jenis_kelas = "Jenis kelas wajib diisi";
   }
-  if (!values.durasi) {
-    errors.durasi = "durasi is required";
+  if (values.jenis_kelas === "group" && values.user_group?.length < 2) {
+    errors.user_group = "Kelas group minimal 2 murid";
   }
   return errors;
 }
